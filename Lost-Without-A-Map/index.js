933186636f84be63bc6884787fbcc0ec6a93f1e5
Jesse-Tingle <jtingle0@gmail.com>
2019-09-07 01:04:21 -0400

    add solution to challenge in index.js file

---
+++ Lost-Without-A-Map/index.js
@@ -0,0 +1,12 @@
+function maps(x){
+    const newArray = x.map(y => y * 2)
+    return newArray
+}
+
+// console.log(maps([1, 2, 3]))
+// console.log(maps([4, 1, 1, 1, 4]))
+// console.log(maps([2, 2, 2, 2, 2, 2]))
+
+maps([1, 2, 3])
+maps([4, 1, 1, 1, 4])
+maps([2, 2, 2, 2, 2, 2])
