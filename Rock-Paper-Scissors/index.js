cb9bc5580f96b67e5dd070dd77fa04ea02f9bba9
Jesse-Tingle <jtingle0@gmail.com>
2019-09-07 02:27:22 -0400

    remove un-needed comments in index.js

--- Rock-Paper-Scissors/index.js
+++ Rock-Paper-Scissors/index.js
@@ -1,22 +1,3 @@
-// const rps = (p1, p2) => {
-//     if (p1 == 'rock' && p2 == 'scissors') {
-//       return 'Player 1 won!'
-//     } else if (p1 == 'scissors' && p2 == 'paper') {
-//       return 'Player 1 won!'
-//     } else if (p1 == 'paper' && p2 == 'rock') {
-//       return 'Player 1 won!'
-//     } else if (p1 == 'scissors' && p2 == 'rock') {
-//       return 'Player 2 won!'
-//     } else if (p1 == 'paper' && p2 == 'scissors') {
-//       return 'Player 2 won!'
-//     } else if (p1 == 'rock' && p2 == 'paper') {
-//       return 'Player 2 won!'
-//     } else {
-//       return 'Draw!'
-//     } 
-//    };
-
-
 const rps = (p1, p2) => {
     if (p1 === p2) return "Draw!";
     var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
@@ -31,15 +12,15 @@ const rps = (p1, p2) => {
 
 
 console.log(rps('rock', 'scissors'))
-// console.log(rps('scissors', 'paper'))
-// console.log(rps('paper', 'rock'))
+console.log(rps('scissors', 'paper'))
+console.log(rps('paper', 'rock'))
 
   
-// console.log(rps('scissors', 'rock'))
-// console.log(rps('paper', 'scissors'))
-// console.log(rps('rock', 'paper'))
+console.log(rps('scissors', 'rock'))
+console.log(rps('paper', 'scissors'))
+console.log(rps('rock', 'paper'))
 
   
-// console.log(rps('rock', 'rock'))
-// console.log(rps('scissors', 'scissors'))
-// console.log(rps('paper', 'paper'))
+console.log(rps('rock', 'rock'))
+console.log(rps('scissors', 'scissors'))
+console.log(rps('paper', 'paper'))
